e57820c3e391de1dcf508eebdce5ad28ae817a03
Gokhan Gok <github-sgokhangok@gmail.com>
2026-05-04 20:31:12 +0300

    Vite base ayari NewStudyArena olarak guncellendi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/StudyArena/",
+  //base: "/StudyArena/",
+  base: "/NewStudyArena/",
   server: {
     port: 5174,
     strictPort: true
